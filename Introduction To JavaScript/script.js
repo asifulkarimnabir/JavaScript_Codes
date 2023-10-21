b900e9a7fe0a50fe6f6824c4b6799d57cd00669a
asifulkarimnabir <asifulkarimnabir17@gmail.com>
2023-10-21 12:04:08 +0600

    first commit

---
+++ Introduction To JavaScript/script.js
@@ -0,0 +1,12 @@
+let c = 4;
+let d = 6;
+
+let _age = 23;
+let name1 = "Nabir";
+
+const $gender = "male";
+
+console.log("c+d is =", a + b);
+console.log("age", _age);
+console.log("name", name1);
+console.log("gender", $gender);
