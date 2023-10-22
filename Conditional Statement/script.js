a43a32d58a512edbc71d70ffc89b8a12b54237f2
asifulkarimnabir <asifulkarimnabir17@gmail.com>
2023-10-22 10:17:01 +0600

    add conditional statement script

---
+++ Conditional Statement/script.js
@@ -0,0 +1,44 @@
+// -----If-Else----
+
+// let age = 13;
+// if (age >= 18) {
+//   console.log("you can vote");
+//   console.log("you are great");
+// } else {
+//   console.log("you can not vote");
+// }
+
+//----- Nested If-Else-----
+
+// let age = 44;
+// if (age >= 18) {
+//   console.log("you can vote");
+//   if (age >= 44) {
+//     console.log("you can vote again");
+//   }
+// } else {
+//   console.log("you can not vote");
+// }
+
+//----- Else-If-----
+
+// let marks = 84;
+// if (marks > 80) {
+//   console.log("A+");
+// } else if (marks > 70) {
+//   console.log("A");
+// } else if (marks > 60) {
+//   console.log("B");
+// } else if (marks > 50) {
+//   console.log("C");
+// } else if (marks > 40) {
+//   console.log("D");
+// } else {
+//   console.log("failed");
+// }
+
+// Ternary Operator
+
+const marks = 23;
+const result = marks >= 45 ? "passed" : "failed";
+console.log(result);
