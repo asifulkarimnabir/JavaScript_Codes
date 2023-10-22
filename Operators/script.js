b7cd5ad68437f5676c7a8a76bbbf37017179dc17
asifulkarimnabir <asifulkarimnabir17@gmail.com>
2023-10-22 09:55:27 +0600

    add operator script

---
+++ Operators/script.js
@@ -0,0 +1,44 @@
+// -----Arithmetic Operator-----
+let firstNumber = 4;
+let secondNumber = 6;
+
+// addition = firstNumber + secondNumber;
+// substraction = firstNumber - secondNumber;
+// multiplication = firstNumber * secondNumber;
+// division = firstNumber / secondNumber;
+result = firstNumber ** secondNumber;
+
+console.log(result);
+
+//------ Assignment Operator--------
+let price = 20;
+// price += 5;
+// price -= 5;
+// price *= 5;
+price /= 5;
+
+console.log(price);
+
+// Comparison Operator
+const a = 5;
+const b = 6;
+
+// const compare = a == b;
+// const compare = a != b;
+// const compare = a < b;
+// const compare = a > b;
+// const compare = a <= b;
+// const compare = a >= b;
+const compare = a <= b;
+// const compare = a === b;
+
+console.log(compare);
+
+// Logical  Operator
+const c = 5;
+const d = 6;
+
+// const logicals = c == d && c < d;
+const logicals = c == d || c < d;
+
+console.log(logicals);
