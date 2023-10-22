2a175794a3098f4b3fcbaaed84a6921594af87f3
asifulkarimnabir <asifulkarimnabir17@gmail.com>
2023-10-22 10:45:46 +0600

    add switch statement script

---
+++ Switch Statement/script.js
@@ -0,0 +1,17 @@
+// switch statement
+
+const grade = "C";
+
+switch (grade) {
+  case "A":
+    console.log("A - Very Good.");
+    break;
+  case "B":
+    console.log("B - Need Improvement.");
+    break;
+  case "C":
+    console.log("C - Not Good.");
+    break;
+  default:
+    console.log("failed");
+}
